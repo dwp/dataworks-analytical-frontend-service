647a56af7e09e0003f87d1004108763f90e068ea
Hamad Ali <hamad.ali@dmwgroup.co.uk>
2020-04-23 09:26:09 +0100

    sending token to orchestration service success full

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+require('dotenv').config()
 
 class MainPage extends Component {  
+
     render() {
         return(
             <div>
                <button id="signIn">
-                  <a href="link" id="proposition-name">Sign in</a>
+                  <a href="{link} "id="proposition-name">Sign in</a>
                </button>
             <p>Main page</p>    
             </div>
--- src/components/waitingpage.js
+++ src/components/waitingpage.js
@@ -2,27 +2,41 @@ import React, {Component} from 'react';
 
 class WaitingPage extends Component {
     
-        getUrlVars = (vars) =>   {
-            window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-            vars[key] = value;
-        });
+async postData(url = '', data = {},token) {
+    // Default options are marked with *
+    
+    const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorisation':token
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+    return response.json(); // parses JSON response into native JavaScript objects
     }
-        sendData(vars){
-            console.log(vars.access_token)
-        }
-
-
+  
+    getUrlVars = (vars) =>   {
+        window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    }
     render() {
         let vars = {};
         this.getUrlVars(vars)
+        this.postData('http://localhost:8080/connect', {}, vars.access_token)
+        .then((data) => {
+        console.log(data); // JSON data parsed by `response.json()` call
+    });
+
         return(
             <div>
                 <p>Your access_token is:</p>
                 {vars.access_token.toString()}
              <p>Call Back page</p>   
-             <button id="spinUp">
-                  <a id="spinUpLink" href="" >Spin up your Clusters</a>
-               </button> 
+             <button id="spinUp">Spin up your Clusters</button> 
             </div>
         )
     }
