037546bc44d6ac551c59fcf8e6c53e0e04852808
ian.utley <ian.utley@infinityworks.com>
2020-05-04 08:55:51 +0100

    Externalise region

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Auth } from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react';
+import getConfig from './utils/appConfig';
 
 Auth.configure(
     {
-        region: 'eu-west-2', 
-        userPoolId: 'eu-west-2_KQ9FANob7', 
-        userPoolWebClientId: '2nkhasknq4qt3egjoe52iuk2vg',
+        region: getConfig('REACT_APP_REGION'),
+        userPoolId: getConfig('REACT_APP_USERPOOLID'), 
+        userPoolWebClientId: getConfig('REACT_APP_USERPOOL_WEBCLIENTID'),
         mandatorySignIn: true,
 
         authenticationFlowType: 'USER_PASSWORD_AUTH',
