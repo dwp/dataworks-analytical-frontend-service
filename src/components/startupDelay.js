5502c15ad70711101281415163c04a9971b8e2ca
Hamad Ali <hammadali248@gmail.com>
2020-05-06 09:20:18 +0100

    added error display on UI

--- src/components/startupDelay.js
+++ src/components/startupDelay.js
@@ -1,33 +1,30 @@
 import React, {Component} from 'react';
 import { ProgressBar } from 'react-mdl';
 
-export default class MainPage extends Component {
+export default class handleClusterDelay extends Component {
     state = {
-        minutes: 5,
-        seconds: 0
+        minutes: 0,
+        seconds: 10,
+        status:"Please wait for For clusters to Spin up",
+        waitTime:"Estimate Wait Time"
       }
       setTimer(){
             if (this.state.seconds > 0){
                 this.setState({seconds:this.state.seconds-1})
-                console.log("seconds>)")
             }else if (this.state.seconds === 0) {
                         if (this.state.minutes === 0) {
-                            console.log("Error in Loading custers")
-                            return ;
+                          this.setState({status:"Error in Loading custers",waitTime:""})
+                          return;
                         } else {
                         this.setState({minutes:this.state.minutes-1,seconds: 59})
-                        console.log("seconds == 0 min min>0")
                         }   
-            }   
-        console.log(this.state.minutes+":"+this.state.seconds)
-    }
-
-    componentDidMount() {
-            this.timer = setInterval(()=> this.getClusterUrl(), 10000); 
-            this.timer = setInterval(()=> this.setTimer(), 1000);
-        
-      
+            }
       }
+
+      componentDidMount() {
+              this.timer = setInterval(()=> this.getClusterUrl(), 10000); 
+              this.timer = setInterval(()=> this.setTimer(), 1000);
+        }
       
       componentWillUnmount() {
         this.timer = null; 
@@ -38,18 +35,20 @@ export default class MainPage extends Component {
         .then(function(response){
             if(response.status === 200 || response.status === 503 || response.status === 502){
                 console.log(response.body);
-         }else{
-           return "error"
-       }
-      })
-    }
-    render(){
+            }else{
+              return "error"
+            }
+        })
+      }
+        
+      render(){
           return (
-          <div className="progressStatus">
-            <h1>Please wait for For clusters to Spin up</h1>
-            <h2>Estimate Wait Time {this.state.minutes}:{this.state.seconds < 10 ? `0${this.state.seconds}` : this.state.seconds}</h2>
-            <ProgressBar indeterminate />
-          </div>
-        );
+                <div className="progressStatus">
+                  <h1>{this.state.status}</h1>
+                  <h2>{this.state.waitTime} {this.state.minutes}:{this.state.seconds < 10 ? `0${this.state.seconds}` : this.state.seconds}</h2>
+                  <ProgressBar indeterminate />
+                </div>
+                
+          )
       }
 }
