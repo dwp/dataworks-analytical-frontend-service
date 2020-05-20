4d5a307cf2799018f60f238c1c8fefdc5249dece
MattToczek <toczek.matt@gmail.com>
2020-05-20 14:58:20 +0100

    removing conflict tag

--- terraform/modules/custom-auth-flow/variables.tf
+++ terraform/modules/custom-auth-flow/variables.tf
@@ -14,11 +14,7 @@ variable "region" {
 }
 
 variable "account" {
-<<<<<<< HEAD
   type        = string
-=======
-  type        = map(string)
->>>>>>> 1d9b461db3e0e68141b9031d6bf586667990bfba
   description = "(Required) AWS account number"
 }
 
