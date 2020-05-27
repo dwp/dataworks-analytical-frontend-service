03acb9bb30a533a52f21184fc3b74e4879725d60
caseyr232 <2818278+caseyr232@users.noreply.github.com>
2020-05-27 12:12:03 +0100

    DW-3938_Update_Cert_Expiry

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 ## Script to generate SSL certificates and then start the app
 
 mkdir -p /etc/${APP_NAME}
-/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/${APP_NAME}/key.pem -out /etc/${APP_NAME}/cert.pem -days 3650 -nodes -subj '/CN=${APP_NAME}'
+/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/${APP_NAME}/key.pem -out /etc/${APP_NAME}/cert.pem -days 30 -nodes -subj '/CN=${APP_NAME}'
 
 node server-build/index.js
