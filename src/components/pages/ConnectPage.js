f1d08ef9eca39f5bb358c3ce5b36d9a9957495a9
Dragos Costache <dragos.costache@infinityworks.com>
2020-06-09 14:07:48 +0100

    Fix Connect page UX issues

--- src/components/pages/ConnectPage.js
+++ src/components/pages/ConnectPage.js
@@ -1,29 +1,43 @@
-import React from "react";
+import React, {useContext} from "react";
 import {ProgressBar} from "react-mdl";
 import {Pages} from "../NavigationComponent";
+import {AuthContext} from "../../utils/Auth";
 
 const ConnectPage = ({nav, desktopUrl}) => {
+    const authContext = useContext(AuthContext);
     const checkIsEnvironmentReady = async () => {
-        const status = await fetch(desktopUrl).then(res => res.status);
-        if (status === 200) {
-            clearInterval(checkInterval);
-            return nav.go(Pages.DESKTOP, {desktopUrl})
-        }
+        try {
+            const status = await fetch(desktopUrl).then(res => res.status);
+            if (status === 200) {
+                clearInterval(checkInterval);
+                return nav.go(Pages.DESKTOP, {desktopUrl})
+            }
 
-        if (Date.now() > timeOutAt) {
+            if (Date.now() > timeOutAt) {
+                clearInterval(checkInterval);
+                authContext.dispatchAuthToast('Timeout reached while trying to connect to the Analytical Environment. Please try again later.');
+                return nav.go(Pages.MAIN);
+            }
+        } catch (e) {
             clearInterval(checkInterval);
-            throw new Error('Analytical Environment provisioning timeout')
+            authContext.dispatchAuthToast('Error occurred when trying to connect to Analytical Environment. Please try again later.');
+            console.error(JSON.stringify(e));
+            return nav.go(Pages.MAIN);
         }
 
     }
 
     const timeOutAt = Date.now() + 5 * 60 * 1000 // 5 minutes
     const checkInterval = setInterval(checkIsEnvironmentReady, 10 * 1000) // 10 seconds
-    checkIsEnvironmentReady()
+    checkIsEnvironmentReady();
 
     return (
-        <div className="progressStatus">
-            <h2>Waiting for Analytical Environment to be provisioned</h2>
+        <div className="progressStatus" style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}>
+            <h3>Waiting for Analytical Environment to be provisioned</h3>
             <ProgressBar indeterminate/>
         </div>
     )
