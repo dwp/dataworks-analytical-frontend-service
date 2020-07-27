358391b0a350e07e1a23ac80771edf7099f59be6
Dragos Costache <dragos.costache@infinityworks.com>
2020-07-27 10:58:14 +0100

    Fix tests

--- src/components/pages/MainPage.spec.js
+++ src/components/pages/MainPage.spec.js
@@ -21,22 +21,6 @@ const MockAuthProvider = ({children}) => <AuthContext.Provider
     value={authMock.AuthHelper()}>{children}</AuthContext.Provider>;
 
 describe("<MainPage/>", () => {
-    it("Creates environment on button click", async (done) => {
-        jest.spyOn(React, 'useEffect').mockImplementation(f => f());
-
-        let wrapper;
-        await act(async () => {
-            wrapper = mount(<MockAuthProvider><MainPage nav={mockNav}/></MockAuthProvider>)
-        });
-        process.nextTick(() => {
-            act(() => {
-                wrapper.update();
-                wrapper.find(Button).simulate('click');
-            });
-            expect(createEnvironment).toBeCalledTimes(1);
-            done();
-        });
-    });
 
     it("Redirects to Setup MFA page if user does not have MFA set up", async (done) => {
         authMock.getCurrentUser.mockImplementation(() => ({preferredMFA: "NONE"}))
