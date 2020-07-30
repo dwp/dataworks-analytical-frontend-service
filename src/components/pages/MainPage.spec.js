e675eb19ce449497cd32dbc3c5372e21fc036f85
Dragos Costache <dragos.costache@infinityworks.com>
2020-07-30 18:54:44 +0100

    Update tests

--- src/components/pages/MainPage.spec.js
+++ src/components/pages/MainPage.spec.js
@@ -21,9 +21,11 @@ const MockAuthProvider = ({children}) => <AuthContext.Provider
     value={authMock.AuthHelper()}>{children}</AuthContext.Provider>;
 
 describe("<MainPage/>", () => {
+    beforeEach(() => jest.clearAllMocks());
 
     it("Redirects to Setup MFA page if user does not have MFA set up", async (done) => {
         authMock.getCurrentUser.mockImplementation(() => ({preferredMFA: "NONE"}))
+        authMock.isFederated.mockImplementation(() => false)
         const wrapper = mount(<MockAuthProvider><MainPage nav={mockNav}/></MockAuthProvider>)
 
         process.nextTick(() => {
@@ -31,6 +33,20 @@ describe("<MainPage/>", () => {
                 wrapper.update();
             });
             expect(mockNav.go).toBeCalledWith(Pages.SETUP_MFA);
+            done();
+        });
+    });
+
+    it("Does not redirect to Setup MFA page if user is federated", async (done) => {
+        authMock.getCurrentUser.mockImplementation(() => ({preferredMFA: "NONE"}))
+        authMock.isFederated.mockImplementation(() => true)
+        const wrapper = mount(<MockAuthProvider><MainPage nav={mockNav}/></MockAuthProvider>)
+
+        process.nextTick(() => {
+            act(() => {
+                wrapper.update();
+            });
+            expect(mockNav.go).toBeCalledTimes(0);
             done();
         });
     });
