483dfd1d4e04edeca951652f1277780e0bea4630
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-28 11:27:07 +0100

    Convert bootstrap to secrets manager (#150)

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -7,27 +7,34 @@ import os
 import sys
 import yaml
 import glob
+import json
 
 
 def main():
     if 'AWS_PROFILE' in os.environ:
         boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
     if 'AWS_REGION' in os.environ:
-        ssm = boto3.client('ssm', region_name=os.environ['AWS_REGION'])
+        secrets_manager = boto3.client(
+            'secretsmanager', region_name=os.environ['AWS_REGION'])
     else:
-        ssm = boto3.client('ssm')
+        secrets_manager = boto3.client('secretsmanager')
 
     try:
-        parameter = ssm.get_parameter(Name='terraform_bootstrap_config', WithDecryption=False)
+        response = secrets_manager.get_secret_value(
+            SecretId="/concourse/dataworks/terraform")
     except botocore.exceptions.ClientError as e:
         error_message = e.response["Error"]["Message"]
         if "The security token included in the request is invalid" in error_message:
-            print("ERROR: Invalid security token used when calling AWS SSM. Have you run `aws-sts` recently?")
+            print("ERROR: Invalid security token used when calling AWS Secrets Manager. Have you run `aws-sts` recently?")
         else:
-            print("ERROR: Problem calling AWS SSM: {}".format(error_message))
+            print("ERROR: Problem calling AWS Secrets Manager: {}".format(
+                error_message))
         sys.exit(1)
 
-    config_data = yaml.load(parameter['Parameter']['Value'], Loader=yaml.FullLoader)
+    config_data = yaml.load(
+        response['SecretBinary'], Loader=yaml.FullLoader)
+    config_data['terraform'] = json.loads(
+        response['SecretBinary'])["terraform"]
     
     j2_files = glob.glob('**/*.j2', recursive=True)
 
