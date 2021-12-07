d681f0a0f711c2b4d42a04ad5a7db0519050a518
rbryett <rory.bryett@gmail.com>
2021-12-07 13:38:40 +0000

    DW-7639 increment the version of container in test envs to latest

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -285,9 +285,9 @@ variable "container_image_tag" {
   description = "(Optional) Override the front-end service docker image tag version in each environment"
   type        = map
   default = {
-    development = "0.0.106"
-    qa          = "0.0.106"
-    integration = "0.0.106"
+    development = "0.0.107"
+    qa          = "0.0.107"
+    integration = "0.0.107"
     preprod     = "0.0.100"
     production  = "0.0.100"
   }
