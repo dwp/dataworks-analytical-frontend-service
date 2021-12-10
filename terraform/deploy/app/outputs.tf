e6f2e6bdc1299e933bb04447fd376757b1b3073a
rbryett <13303701+rbryett@users.noreply.github.com>
2021-12-10 09:49:18 +0000

    Dw 7639 (#170)
    * DW-7639 add prometheus middleware with collectDefaultMetrics client to begin with@
    
    * DW-7639 use core client to allow for custom metrics to be sent
    
    * DW-7638 adding a custom metric with labels to monitor response codes
    
    * DW-7638 adding a custom metric with labels to monitor response codes
    
    * DW-7638 adding a custom metric with labels to monitor response codes
    
    * DW-7638 adding a custom metric with labels to monitor response codes
    
    * DW-7638 adding a custom metric with labels to monitor response codes
    
    * DW-7639 timeout logic serverside with terraform monitoring
    
    * DW-7639 remove the timeout logic clientside and rely on 504 gateway timeout to represent timed out requests to analytical env
    
    * DW-7639 remove the timeout logic clientside and rely on 504 gateway timeout to represent timed out requests to analytical env
    
    * DW-7639 update test to reflect 504 gateway timeout representing timed out requests to analytical env
    
    * DW-7639 test to see if this fixes vulnerability
    
    * DW-7639 revert
    
    * Revert "DW-7639 revert"
    
    This reverts commit 6c89bc2f3a68473b3ee9d05797eee3fa2d10e78c.
    
    * Revert "DW-7639 test to see if this fixes vulnerability"
    
    This reverts commit 17709047c3bd3c89b7bd04f740be735774894e75.
    
    * DW-7639 increment the version of container in test envs to latest
    
    * DW-7639 fix the population of aws region variable
    
    * DW-7639 reimplement timeout logic alongside 504
    
    * DW-7639 reimplement timeout logic alongside 504
    
    * DW-7639 increment docker container
    
    * DW-7639 increment docker container
    
    * increment docker version
    
    * DW-7639 increment docker image for prod and preprod
    
    * DW-7639 adding service discovery for prometheus to scrape both analytical frontend service containers
    
    * squash
    
    * syntax

--- terraform/deploy/app/outputs.tf
+++ terraform/deploy/app/outputs.tf
@@ -4,3 +4,12 @@ output "app_client" {
     id           = module.cognito-app.outputs.app_client.id
   }
 }
+
+output "frontend_service" {
+  value = {
+    service_port = var.container_port
+    sg_id = module.ecs-fargate-service.ecs_tasks.id
+    frontend_service_discovery_dns = aws_service_discovery_private_dns_namespace.analytical_frontend_service
+    frontend_service_discovery     = aws_service_discovery_service.analytical_frontend_service
+  }
+}
