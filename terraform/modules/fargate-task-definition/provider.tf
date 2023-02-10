02285059735a0e6ffb01ac5301b5bcfb89c29301
jonnym <jon.melia@cloudadept.co.uk>
2023-02-10 15:24:46 +0000

    Dtwrks 695 (#209)
    * Missed tags
    
    * Fixing tags
    
    * Fixing tags
    
    * Fixing tags
    
    * Still fixing tags
    
    * More tagging
    
    * More tagging
    
    ---------
    
    Co-authored-by: Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>

--- terraform/modules/fargate-task-definition/provider.tf
+++ terraform/modules/fargate-task-definition/provider.tf
@@ -2,6 +2,17 @@ provider "aws" {
   alias  = "management"
   region = "eu-west-2"
 
+  default_tags {
+    tags = {
+      Application      = "DataWorks"                              # As per our definition on ServiceNow
+      Function         = "Data and Analytics"                     # As per our definition on ServiceNow
+      Environment      = local.hcs_environment[local.environment] # Set up locals as per Tagging doc requirements https://engineering.dwp.gov.uk/policies/hcs-cloud-hosting-policies/resource-identification-tagging/
+      Business-Project = "PRJ0022507"                             # This seems to replace costcode as per the doc https://engineering.dwp.gov.uk/policies/hcs-cloud-hosting-policies/resource-identification-tagging/
+      AutoShutdown     = "False"
+      Persistence      = "True"
+      Costcode         = "PRJ0022507"
+    }
+  }
   assume_role {
     role_arn = var.management_role_arn
   }
