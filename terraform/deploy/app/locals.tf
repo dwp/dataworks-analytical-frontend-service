28a7ffc3c4825e0023c3861caed38648dafef26b
jonnym <jon.melia@cloudadept.co.uk>
2023-02-10 11:47:44 +0000

    AWS Provider 4.54 and Tagging (#204)
    * AWS Provider 4.54 and Tagging
    
    * AWS Provider 4.54 and Tagging
    
    ---------
    
    Co-authored-by: Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>

--- terraform/deploy/app/locals.tf
+++ terraform/deploy/app/locals.tf
@@ -13,4 +13,16 @@ locals {
     },
   ]
   ecr_endpoint = "${local.account[local.management_account[local.environment]]}.dkr.ecr.${var.region}.amazonaws.com"
+
+  # Configured as per Tagging doc requirements https://engineering.dwp.gov.uk/policies/hcs-cloud-hosting-policies/resource-identification-tagging/
+  # Also required as per Tenable documentation https://engineering.dwp.gov.uk/products/gold-images/agents/tenable/
+  hcs_environment = {
+    development    = "Dev"
+    qa             = "Test"
+    integration    = "Stage"
+    preprod        = "Stage"
+    production     = "Production"
+    management     = "SP_Tooling"
+    management-dev = "DT_Tooling"
+  }
 }
