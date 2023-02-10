a92f5d0373ba8101c734a1bb59d8b208c0ad45f7
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-02-10 11:53:56 +0000

    Missed tags

--- terraform/modules/fargate-task-definition/locals.tf
+++ terraform/modules/fargate-task-definition/locals.tf
@@ -6,4 +6,16 @@ locals {
       "protocol"      = "tcp"
     },
   ]
+
+  # Configured as per Tagging doc requirements https://engineering.dwp.gov.uk/policies/hcs-cloud-hosting-policies/resource-identification-tagging/
+  # Also required as per Tenable documentation https://engineering.dwp.gov.uk/products/gold-images/agents/tenable/
+  hcs_environment = {
+    development    = "Dev"
+    qa             = "Test"
+    integration    = "Stage"
+    preprod        = "Stage"
+    production     = "Production"
+    management     = "SP_Tooling"
+    management-dev = "DT_Tooling"
+  }
 }
